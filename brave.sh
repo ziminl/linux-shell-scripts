9b85d449ba8c61121e929a5256af0192641ac9b3
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-07-14 21:03:51 +0900

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# Read the contents of /etc/os-release into variables
+source /etc/os-release
+
+# Check for Debian-based distributions
+if [[ $ID == "debian" || $ID == "ubuntu" || $ID == "linuxmint" ]]; then
+    echo "Debian-based"
+elif [[ $ID == "fedora" || $ID == "centos" || $ID == "rhel" || $ID == "rocky" ]]; then
+    echo "script is for debian ubuntu mint"
+else
+    echo "other. cant install"
+fi
+
+
+
+
 sudo apt install curl
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
